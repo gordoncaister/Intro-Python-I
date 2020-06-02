4bae3466a37b0b44e9bf5c0d428450dd6eac6eb0
Gordon Caister <gordoncaister@gmail.com>
2020-06-02 13:30:13 -0500

    completed 05

--- src/05_lists.py
+++ src/05_lists.py
@@ -18,14 +18,19 @@ print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
+x.insert((len(x)-1),99)
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for number in x:
+    print(number*1000)
